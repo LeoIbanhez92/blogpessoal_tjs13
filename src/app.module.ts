84beb6c953b3063fb7a7a75e659b64e5a1e04e39
Leonardo <leonardohibanhez@gmail.com>
2026-03-16 17:30:19 -0300

    🐛 FIX: Correção

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostagemModule } from './postagem/postagem.module';
-import { Postagem } from './postagem/entities/postagem.entity';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';

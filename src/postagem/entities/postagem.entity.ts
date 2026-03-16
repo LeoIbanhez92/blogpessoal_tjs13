cabee43f27c43dec55efca719eecbaad14f4a530
Leonardo <leonardohibanhez@gmail.com>
2026-03-16 17:23:47 -0300

    🚀 Deploy:  Deploy do Projeto Blog Pessoal

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -3,33 +3,40 @@ import { IsNotEmpty, Length } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
 import { Usuario } from "../../usuario/entities/usuario.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({ name: "tb_postagens" }) //! CREATE TABLE tb_postagens
 export class Postagem {
 
+    @ApiProperty()
     @PrimaryGeneratedColumn() //! PRIMARY KEY(ID) AUTO INCREMENT
     id: number;
 
+    @ApiProperty()
     @Transform(({ value }: TransformFnParams) => value?.trim()) //! REMOVER ESPAÇOS EM BRANCO (INICIO / FIM)
     @IsNotEmpty() //! ELE FORÇA A DIGITAÇÃO
     @Length(5, 100, { message: "O Texto deve ter entre 5 e 100 caracteres" })
     @Column({ length: 100, nullable: false }) //! VARCHAR (100) NOT NULL
     titulo: string;
 
+    @ApiProperty()
     @Transform(({ value }: TransformFnParams) => value?.trim()) //! REMOVER ESPAÇOS EM BRANCO (INICIO / FIM)
     @IsNotEmpty() //! ELE FORÇA A DIGITAÇÃO
     @Length(10, 1000, { message: "O Texto deve ter entre 10 e 1000 caracteres" })
     @Column({ length: 1000, nullable: false }) //! VARCHAR (1000) NOT NULL
     texto: string;
 
+    @ApiProperty()
     @UpdateDateColumn() //! ATUALIZA A DATA NA CRIAÇÃO E NA ATUALIZAÇÃO
     data: Date;
 
+    @ApiProperty({ type: () => Tema })
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE"
     })
     tema: Tema; 
 
+    @ApiProperty({ type: () => Usuario })
     @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
         onDelete: "CASCADE"
     })
--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -2,19 +2,23 @@ import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty, Length } from "class-validator";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({ name: "tb_temas" })
 export class Tema {
 
     @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number;
 
     @Transform(({ value }: TransformFnParams) => value?.trim()) //! REMOVER ESPAÇOS EM BRANCO (INICIO / FIM)
     @IsNotEmpty() //! ELE FORÇA A DIGITAÇÃO
     @Length(5, 255, { message: "O Texto deve ter entre 5 e 255 caracteres" }) //! FORÇA A DIGITAÇÃO COM LIMITE DE 50 E MAXIMA DE 255 CARACTERES
     @Column({ length: 255, nullable: false }) //! VARCHAR (255) NOT NULL
+    @ApiProperty()
     descricao: string;
 
+    @ApiProperty()
     @OneToMany(() => Postagem, (postagem) => postagem.tema)
 
     postagem: Postagem[]; //! ARRAY DE RETORNO
